735397f3ffdaec3d555ccf62fdda2844cb74eac0
kovs705 <smartkovsik@gmail.com>
2022-08-17 00:27:54 +0300

    placed arranged subview in stack on center

--- My progress/Views/AddProgressVC.swift
+++ My progress/Views/AddProgressVC.swift
@@ -23,10 +23,12 @@ class AddProgressVC: UIViewController {
         scrollPage.showsVerticalScrollIndicator = true
         scrollPage.bounces = true
         scrollPage.alwaysBounceVertical = true
-        scrollPage.contentSize = CGSize(width: Int(self.view.frame.size.width), height: 100)
+        // scrollPage.contentSize = CGSize(width: Int(self.view.frame.size.width), height: 100)
         
         stackView.axis = .vertical
         stackView.spacing = 20
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
         
         // MARK: - platform for views:
         view.addSubview(scrollPage)
@@ -44,21 +46,27 @@ class AddProgressVC: UIViewController {
         
         stackView.snp.makeConstraints { (stack) -> Void in
             stack.top.equalTo(scrollPage.snp.top)
-            stack.leading.equalTo(scrollPage.snp.leading)
-            stack.bottom.right.equalTo(scrollPage)
+            stack.leading.equalTo(view.snp.leading).offset(20)
+            stack.bottom.equalTo(scrollPage)
+            // stack.width.equalTo(scrollPage.snp.width).offset(-40)
+            stack.centerX.equalToSuperview()
         }
         
         // self.view = view
         
         // MARK: - stack's pbjects:
-        for _ in 0..<20 {
+        for _ in 0..<1 {
             let circle = UIView()
             circle.translatesAutoresizingMaskIntoConstraints = true
+            
             circle.snp.makeConstraints { (circle) -> Void in
                 circle.width.equalTo(50)
                 circle.height.equalTo(50)
             }
-            circle.backgroundColor = .gray
+            // circle.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            // circle.widthAnchor.constraint(equalToConstant: 50).isActive = true
+            
+            circle.backgroundColor = .white
             circle.layer.cornerRadius = 24
             
             stackView.addArrangedSubview(circle)
